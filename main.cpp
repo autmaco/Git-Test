77135beeafc13254d068d87fe0bc7b179ed6c21b
Autumn <autmaco@ut.utm.edu>
2018-04-02 18:20:20 -0500

    New main.cpp.

--- main.cpp
+++ main.cpp
@@ -7,8 +7,26 @@
 
 using namespace std;
 
+//function
+int sum(int num){
+  int sum =0;
+  for(int i =1; i <=num; i++){
+    sum +=i;
+  }
+  return sum;
+}
+
+
 int main(){
 
+  int n;
+  
   cout << "Hello World!" << endl;
 
+  cout << "Please enter a number!" <<endl;
+  cin >> n;
+
+  cout << "The sum is: " << sum(n) << endl; 
+  //sum(n);
 }
+
